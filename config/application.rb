eec37b9b4f3bf6135fdd83d20b90de6a24b3253e
Christina Cannito <christinacannito@gmail.com>
2014-08-22 10:29:55 -0400

    updated application

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module LaunchBox
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.assets.initialize_on_precompile = false
   end
 end
