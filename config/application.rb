cfad49e7c2dccade6570825a77aab7f8869049c2
Christina Cannito <christinacannito@gmail.com>
2014-08-22 10:58:46 -0400

    updated application.rb file

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,8 @@ module LaunchBox
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    # config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    # config.assets.paths << "#{Rails.root}/app/assets/fonts"
     config.assets.initialize_on_precompile = false
   end
 end
