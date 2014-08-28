4e39ae01343948946f0554429e933320282c5b5d
Sandy <Sandy.L45@gmail.com>
2014-08-28 16:03:01 -0400

    image carousel btn

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,3 +1,15 @@
 // $("div .hover-box").find(".hover-text").hide();
 
-$("div .tools-box .hover-box").find("div .hover-text").hide();
+// $("div .tools-box .hover-box").hover(function(){
+//   $(this).find()
+// });
+
+// $("div .btn").hover(function(){
+//   $(this).find()
+// });
+
+
+$("div .btn").hover(function(){
+   // $(this).find('a img').attr("src", 'images/mouse_over.png');
+   alert(this);
+});
